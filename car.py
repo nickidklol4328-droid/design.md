1eb9a7a91559bbacbb8797f6b2b5422ffb36c70b
nickidklol4328-droid <nickidklol4328@gmail.com>
2026-07-27 22:20:48 +0900

    car.py
    Implement driving functions for the car including forward, backward, turning, and sliding.

---
+++ car.py
@@ -0,0 +1,40 @@
+from motor import MotorController
+from ultrasonic import Ultrasonic
+import time
+
+drive = MotorController()
+
+drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=-1,rear_right=-1)
+
+def drive_fd():
+    drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def drive_bw():
+    drive.set_motors(front_left=-1000,rear_left=-1000,front_right=-1000,rear_right=-1000)
+
+def turn_lt():
+    drive.set_motors(front_left=-1000,rear_left=-1000,front_right=1000,rear_right=1000)
+
+def turn_rt():
+    drive.set_motors(front_left=1000,rear_left=1000,front_right=-1000,rear_right=-1000)
+
+def slide_rt():
+    drive.set_motors(front_left=1000,rear_left=-1000,front_right=-1000,rear_right=1000)
+
+def slide_lt():
+    drive.set_motors(front_left=-1000,rear_left=1000,front_right=1000,rear_right=-1000)
+
+def slide_d_fd_lt():
+    drive.set_motors(front_left=0,rear_left=1000,front_right=1000,rear_right=-0)
+
+def slide_d_bw_lt():
+    drive.set_motors(front_left=-1000,rear_left=0,front_right=0,rear_right=-1000)
+
+def slide_d_fd_rt():
+    drive.set_motors(front_left=1000,rear_left=0,front_right=0,rear_right=1000)
+
+def slide_d_bw_rt():
+    drive.set_motors(front_left=0,rear_left=-1000,front_right=-1000,rear_right=0)
+
+def stop():
+    drive.set_motors(front_left=0,rear_left=0,front_right=0,rear_right=0)
